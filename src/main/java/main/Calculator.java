327a79fe75bafe43e5f731bedabeb8d2a82018c8
User <User@Win7x64>
2019-07-04 20:22:52 +0530

    revised

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -48,7 +48,7 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here
-    	result = x-y;
+    	result = x - y;
         return result;
     }
     public static int mult(int x, int y)
